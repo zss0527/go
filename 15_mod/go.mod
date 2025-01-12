10074dee830bb190370222a3fed1ba58caef61d3
zss0527 <zss0527@outlook.com>
2025-01-12 15:27:30 +0800

    third party packages

--- 15_mod/go.mod
+++ 15_mod/go.mod
@@ -1,3 +1,13 @@
 module itying
 
 go 1.23.0
+
+require (
+	github.com/shopspring/decimal v1.4.0
+	github.com/tidwall/gjson v1.18.0
+)
+
+require (
+	github.com/tidwall/match v1.1.1 // indirect
+	github.com/tidwall/pretty v1.2.0 // indirect
+)
--- 15_mod/main.go
+++ 15_mod/main.go
@@ -5,6 +5,9 @@ import (
 	"itying/calc"
 	T "itying/tools"
 	"itying/tools/config"
+
+	"github.com/shopspring/decimal"
+	"github.com/tidwall/gjson"
 )
 
 func main() {
@@ -13,7 +16,7 @@ func main() {
 	fmt.Println(calc.Aaa)
 	// fmt.Println(calc.aaa)
 	fn1()
-
+	thirdPartyPackageFn()
 }
 
 func fn1() {
@@ -26,4 +29,26 @@ func fn1() {
 // init func run before main
 func init() {
 	fmt.Println("inited in main package...")
+}
+
+/*
+1. import third party packages
+2. go mod tidy to download related dependencies
+*/
+func thirdPartyPackageFn() {
+	n, _ := decimal.NewFromString("-123.4567")
+	fmt.Println(n.String()) // output: "-123.4567"
+
+	json := `{
+		"user":{
+		   "first.name": "Janet",
+		   "last.name": "Prichard"
+		 }
+	}`
+	user := gjson.Get(json, "user")
+	fmt.Println(user.Get(gjson.Escape("first.name")))
+	fmt.Println(user.Get(gjson.Escape("last.name")))
+	// Output:
+	// Janet
+	// Prichard
 }
