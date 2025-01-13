3a750b6e832c82e6fc0b981243d74c222252dd9a
zss0527 <zss0527@outlook.com>
2025-01-13 21:59:16 +0800

    update gin_demo01

--- 20_gin_demo01/main.go
+++ 20_gin_demo01/main.go
@@ -1,29 +1,31 @@
 package main
 
 import (
+	"net/http"
+
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
 	r := gin.Default()
 	r.GET("/", func(context *gin.Context) {
-		context.String(200, "Hello %v!", "Gin")
+		context.String(http.StatusOK, "Hello %v!", "Gin")
 	})
 
 	r.GET("/news", func(ctx *gin.Context) {
-		ctx.String(200, "I am a get request, mainly to get resources.")
+		ctx.String(http.StatusOK, "I am a get request, mainly to get resources.")
 	})
 
 	r.POST("/add", func(ctx *gin.Context) {
-		ctx.String(200, "I am a post request, mainly to add resources.")
+		ctx.String(http.StatusOK, "I am a post request, mainly to add resources.")
 	})
 
 	r.PUT("/edit", func(ctx *gin.Context) {
-		ctx.String(200, "this is an put request, mainly to edit resources.")
+		ctx.String(http.StatusOK, "this is an put request, mainly to edit resources.")
 	})
 
 	r.DELETE("/delete", func(ctx *gin.Context) {
-		ctx.String(200, "I am an delete request, mainly to delete resources.")
+		ctx.String(http.StatusOK, "I am an delete request, mainly to delete resources.")
 	})
 
 	r.Run(":8081")
